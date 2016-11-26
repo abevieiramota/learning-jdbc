f211ab3fefbf7adf370afa71f50c2715b600cab9
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-11-26 14:42:38 -0200

    alura aula 4

--- src/main/java/com/abevieiramota/jdbc/Insert.java
+++ src/main/java/com/abevieiramota/jdbc/Insert.java
@@ -1,7 +1,6 @@
 package com.abevieiramota.jdbc;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,24 +14,41 @@ public class Insert {
 	public static void main(String[] args)
 			throws ClassNotFoundException, SQLException, URISyntaxException, IOException {
 		try (Connection conn = ConnectionFactory.instance();
-				InputStream is = ConnectionFactory.class.getClassLoader().getResourceAsStream("arquivo.csv");
 				PreparedStatement stmt = conn.prepareStatement("insert into pessoa values (?, ?, ?)",
 						Statement.RETURN_GENERATED_KEYS)) {
 
-			// POSIÇÃO COMEÇA EM 1!!!!!!!!!
-			stmt.setString(1, "Abelardo Vieira Mota");
-			stmt.setInt(2, 100);
-			stmt.setDate(3, new java.sql.Date(new Date().getTime()));
-
-			boolean resultado = stmt.execute();
-			System.out.println("Resultado: " + resultado);
-
-			ResultSet resultSet = stmt.getGeneratedKeys();
-
-			while (resultSet.next()) {
-				int id = resultSet.getInt("id");
-				System.out.println("Id gerado: " + id);
+			conn.setAutoCommit(false);
+			try {
+				extracted("Abelardo", 27, new Date(), stmt);
+				extracted("Juliana", 28, new Date(), stmt);
+				conn.commit();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				conn.rollback();
+				System.out.println("rollback efetuado");
 			}
+		}
+	}
+
+	private static void extracted(String nome, int idade, Date dataNascimento, PreparedStatement stmt)
+			throws SQLException {
+
+		if (idade == 28) {
+			throw new IllegalArgumentException("to dando erro");
+		}
+		// POSIÇÃO COMEÇA EM 1!!!!!!!!!
+		stmt.setString(1, nome);
+		stmt.setInt(2, idade);
+		stmt.setDate(3, new java.sql.Date(dataNascimento.getTime()));
+
+		boolean resultado = stmt.execute();
+		System.out.println("Resultado: " + resultado);
+
+		ResultSet resultSet = stmt.getGeneratedKeys();
+
+		while (resultSet.next()) {
+			int id = resultSet.getInt("id");
+			System.out.println("Id gerado: " + id);
 		}
 	}
 }
